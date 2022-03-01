0f414c0a5b01b433130dca60711dc237f2a0ec91
Dev1444 <98470656+Dev1444@users.noreply.github.com>
2022-02-28 23:48:46 -0500

    Update count_longest.py

--- count_longest.py
+++ count_longest.py
@@ -1,25 +1,28 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 4: COUNT THE LONGEST SUBSEQUENCE
 #
-# NAME:         FIXME
+# NAME:         Deven Mistry
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
-from Queue import Queue
-
-# count longest sequence of duplicates in a queue
-# can destroy the queue & make it empty
-def count_longest(q):
-    len = 0
-
-    return len
-
-def main():
-    print("out 2:", count_longest( Queue( [ l for l in "hello" ] ) ))
-    print("out 5:", count_longest(Queue([l for l in "m" * 5])))
-    print("out 3:", count_longest(Queue([l for l in "heee"])))
-
-
-# Don't run main on import
-if __name__ == "__main__":
-    main()
+# function for finding the longest queue
+def count_longest(queue):
+    result = [0]
+    # indexing data
+    for index,data in enumerate(queue):
+        count = 0
+        # iterating through queue
+        for value in queue[index:]:
+            if(value == data):
+                count += 1
+                result.append(count)
+            else:
+                result.append(count)
+                break
+    return max(result)
+ # displaying test cases
+if __name__ == '__main__':
+    print(count_longest(['h','e','l','l','o']))
+    print(count_longest(['m','m','m','m','m']))
+    print(count_longest(['h','e','e','e']))
+    print(count_longest([]))
 
